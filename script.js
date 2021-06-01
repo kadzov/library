bf4c679cfb79e410aa26e53e89fa0e89cc927d74
przrk <76454345+przrk@users.noreply.github.com>
2021-06-02 01:57:30 +0300

    add

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let myLibrary = [];
 const list = document.querySelector('#list');
+const button = document.querySelector('button');
+window.addEventListener('keydown', e => {
+  if (e.code === 'Enter') {
+    button.blur();
+    addBookToLibrary();
+  }
+});
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,9 +14,30 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 function addBookToLibrary() {
-  const data = document.querySelectorAll('[type="text"]')
-  myLibrary.push(new Book(...Array.from(data).map(x => x.value)));
-  const book = document.createElement('div');
-  book.textContent = Object.values(myLibrary[0])
-  list.appendChild(book)
+  const data = document.querySelectorAll('[type="text"]');
+  myLibrary.push(new Book(...Array.from(data).map(i => i.value)));
+  const book = myLibrary[myLibrary.length - 1];
+  const card = document.createElement('div');
+  card.style.position = 'relative';
+  card.innerHTML += `<span class='x'>✖</span>`;
+  card.innerHTML += `<span class='status'>✔</span>`;
+  for (const info in book) {
+    card.innerHTML += `<p>${book[info]}</p>`;
+  }
+  list.appendChild(card);
+  const x = document.querySelectorAll('.x')[myLibrary.length - 1];
+  x.addEventListener('click', () => {
+    card.remove();
+    myLibrary.splice(myLibrary.indexOf(card), 1);
+  });
+  const status = document.querySelectorAll('.status')[myLibrary.length - 1];
+  status.addEventListener('click', e => {
+    if (getComputedStyle(status).color === 'rgba(255, 255, 255, 0.376)') {
+      e.target.style.color = '#26ff00';
+      card.lastChild.textContent = 'read';
+    } else {
+      e.target.style.color = '#ffffff60';
+      card.lastChild.textContent = 'not read yet';
+    }
+  });
 }
